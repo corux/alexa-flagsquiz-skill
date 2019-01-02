3ea95a3d689bfbcf862f3f02dd8ecf18683a83c3
Tobias Wallura <tobias@wallura.org>
2019-01-02 10:38:18 +0100

    setup jest for tests

---
+++ __tests__/HelpIntent.spec.ts
@@ -0,0 +1,21 @@
+import { VirtualAlexa } from "virtual-alexa";
+import { handler } from "../src";
+
+describe("AMAZON.HelpIntent", () => {
+  let alexa: VirtualAlexa;
+  beforeEach(() => {
+    alexa = VirtualAlexa.Builder()
+      .handler(handler)
+      .interactionModelFile("models/de-DE.json")
+      .create();
+    alexa.dynamoDB().mock();
+  });
+
+  it("Provide help message", async () => {
+    await alexa.launch();
+    const result: any = await alexa.utter("help");
+    expect(result.response.outputSpeech.ssml).toContain("dein Wissen über die Länder der Welt testen");
+    expect(result.response.shouldEndSession).toBe(false);
+  });
+
+});
---
+++ __tests__/StopIntent.spec.ts
@@ -0,0 +1,27 @@
+import AWS = require("aws-sdk");
+import { VirtualAlexa } from "virtual-alexa";
+import { handler } from "../src";
+
+describe("AMAZON.StopIntent, AMAZON.CancelIntent", () => {
+  let alexa: VirtualAlexa;
+  beforeEach(() => {
+    alexa = VirtualAlexa.Builder()
+      .handler(handler)
+      .interactionModelFile("models/de-DE.json")
+      .create();
+    alexa.dynamoDB().mock();
+  });
+
+  it("StopIntent ends session", async () => {
+    const result: any = await alexa.utter("stop");
+    expect(result.response.outputSpeech.ssml).toContain("Bis bald");
+    expect(result.response.shouldEndSession).toBe(true);
+  });
+
+  it("CancelIntent ends session", async () => {
+    const result: any = await alexa.utter("cancel");
+    expect(result.response.outputSpeech.ssml).toContain("Bis bald");
+    expect(result.response.shouldEndSession).toBe(true);
+  });
+
+});
